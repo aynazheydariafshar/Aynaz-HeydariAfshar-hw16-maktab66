902cd85a21786818cc7b4ce9c59e815edd1cdaa9
Aynaz HeydariAfshar <aynazhadriafshar@gmail.com>
2022-03-15 18:04:31 +0330

    add bootstrap react

--- ex2/src/App.js
+++ ex2/src/App.js
@@ -3,7 +3,9 @@ import './Asseste/Styles/App.css';
 function App() {
   return (
     <div className="App">
-     
+        <div className='card-center'>
+          <h2>hi</h2>
+        </div>
     </div>
   );
 }
