6179103236f5c1cec0521319e0e19f4edb1c4f33
Aynaz HeydariAfshar <aynazhadriafshar@gmail.com>
2022-03-16 15:21:22 +0330

    add eye icon

--- ex2/src/Hooks/Login.js
+++ ex2/src/Hooks/Login.js
@@ -2,8 +2,19 @@ import React from 'react';
 import {Form} from 'react-bootstrap';
 import '../Asseste/Styles/Login.css'
 import SubmitButton from './SubmitButton';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye } from "@fortawesome/free-solid-svg-icons";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
+    const eye = <FontAwesomeIcon icon={faEye} />;
+    
+    const { login, handleSubmit } = useForm();
+    
+    const onSubmit = data => {
+        console.log(data);
+    };
+    
     return <Form className='p-4'>
         <h4 className='text-light my-4'>خوش آمدید</h4>
         <Form.Control 
@@ -12,15 +23,18 @@ const Login = () => {
             type="email" 
             placeholder="پست الکترونیکی" 
         />
+        <div className='inputpassword-div'>
         <Form.Control 
             size='sm' 
             className='inputPassword my-4' 
             type="password" 
             placeholder="کلمه عبور"
         />
+        <i>{eye}</i>
+        </div>
         <div className='row-flex'>
             <a href="#" class="link-forget mb-3">فراموش کردید ؟</a>
-            <SubmitButton title={'ورود'}/>
+            <SubmitButton handle={handleSubmit(onSubmit)} title={'ورود'}/>
         </div>
     </Form>
 }
