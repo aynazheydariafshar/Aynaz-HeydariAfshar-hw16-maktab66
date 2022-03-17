5e685b299181d7124bd511bb2e63387014f9305b
Aynaz HeydariAfshar <aynazhadriafshar@gmail.com>
2022-03-17 16:44:23 +0330

    get data

--- ex2/src/Hooks/Register.js
+++ ex2/src/Hooks/Register.js
@@ -5,6 +5,10 @@ import '../Asseste/Styles/Register.css'
 
 const Register = () => {
     const [select, stateselect] = useState('choose');
+    const [selectCity, stateselectCity] = useState('chooseCity');
+    const [selectSmalCity, stateselectSmalCity] = useState('chooseSmallCity');
+    const [data, setdata] = useState([]);
+    
 
     const education = ['کاردانی', 'کارشناسی', 'کارشناسی ارشد', 'دکتری' , 'دیپلم'];
 
@@ -12,6 +16,22 @@ const Register = () => {
             
     }
 
+    
+    const fetchData = () => {
+        fetch('/json/iranstates.json')
+        .then((response) => response.json())
+        .then((data) => {
+            setdata(data);
+        })
+        .catch((error) => {
+            alert('error from data')
+        });
+    };
+    
+    useEffect(() => {
+        fetchData();
+    }, []);
+    
     return <Form className='p-4' onSubmit={handleRegisterSubmit}>
         <h5 className='text-light my-4'>رایگان ثپت نام کنید</h5>
         <div className='row'>
@@ -48,11 +68,43 @@ const Register = () => {
         </Form.Control>
         {select !== 'choose' && <Form.Control 
             size='sm' 
-            className= 'register inputeducation'
+            className= 'register inputeducation my-4'
             type="text" 
             placeholder="محل تحصیل" 
             required
         />}
+        <div className='row'>
+            <div className='col-6'>
+                <Form.Control 
+                    className='register selectEducation' 
+                    as="select"
+                    onChange={e => {
+                        stateselectSmalCity(e.target.value)
+                    }}
+                >
+                <option value={'chooseSmallCity'}>شهرستان</option>
+                {Object.keys(data).map((city) => {
+                    data[city].map((item,idx) => {
+                        <option key={idx} value={item}>{item}</option>
+                    })
+                })}
+                </Form.Control>
+            </div>
+            <div className='col-6'>  
+                <Form.Control 
+                    className='register selectEducation' 
+                    as="select"
+                    onChange={e => {
+                        stateselectCity(e.target.value)
+                    }}
+                >
+                <option value={'chooseCity'}>استان</option>
+                {Object.keys(data).map((item,idx) => (
+                    <option value={item}>{item}</option>
+                ))}
+                </Form.Control>
+            </div>
+        </div>    
     </Form>;
 }
 
